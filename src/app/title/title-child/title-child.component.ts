05005d4b2ee967a79eed572c44aa1588e492736d
William Oliveira <1704639@aluno.univesp.br>
2020-07-16 15:33:50 -0300

    Função com .emit
    Acrescentado função de emissão.

--- src/app/title/title-child/title-child.component.ts
+++ src/app/title/title-child/title-child.component.ts
@@ -7,12 +7,14 @@ import {Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class TitleChildComponent implements OnInit {
   @Input() receiveName;
-  @Output() nameFather = new EventEmitter();
+  @Output() nameFather = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.receiveName);
   }
-
+  nameEmit(): void {
+    this.nameFather.emit(this.receiveName);
+  }
 }
